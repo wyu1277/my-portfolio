91d7c08c03168832e7f5313d0085165d69ea15dc
Wilson Yu <wyu1277@gmail.com>
2023-09-18 15:43:58 -0400

    styled Le Waf project page for desktop and mobile

--- src/app/le-waf/page.js
+++ src/app/le-waf/page.js
@@ -4,107 +4,107 @@ export default function LeWaf() {
       <section className="mb-10">
       <img src="/images/LeWafHeroBanner.png" alt="LeWaf Hero Banner"/>
       </section>
-      <section className="lg:px-[250px] max-lg:px-[20px] mb-10">
-      <h1>Le Waf</h1>
-      <h2>Redesigning Le Waf's Premium Retail Waffle Ecommerce Site</h2>
-      <div className="grid grid-flow-col gap-40 justify-center">
+      <section className="lg:px-[250px] max-lg:px-[20px] py-10 mb-10">
+      <h1 className="font-Futura-PT-Bold text-3xl">Le Waf</h1>
+      <h2 className="font-Futura-PT text-xl font-semibold mb-10">Redesigning Le Waf's Premium Retail Waffle Ecommerce Site</h2>
+      <div className="lg:grid lg:grid-flow-col lg:gap-40 justify-center">
         <div>
-          <h3>Client Background</h3>
-          <p>Le Waf is a renowned artisanal waffle brand, known for its premium and handcrafted waffles. They have gained popularity and recognition after being featured in a food network show hosted by Guy Fieri. However, their current website didn't align with their desired premium branding and fails to convey the message of their careful craftsmanship. Le Waf wants a complete website redesign that showcases their commitment to quality and delivers a seamless online shopping experience to customers.</p>
+          <h3 className="font-Futura-PT mb-5 uppercase text-gray-500">Client Background</h3>
+          <p className="font-Futura-PT">Le Waf is a renowned artisanal waffle brand, known for its premium and handcrafted waffles. They have gained popularity and recognition after being featured in a food network show hosted by Guy Fieri. However, their current website didn't align with their desired premium branding and fails to convey the message of their careful craftsmanship. Le Waf wants a complete website redesign that showcases their commitment to quality and delivers a seamless online shopping experience to customers.</p>
         </div>
         <div>
-          <h3>Role</h3>
-          <p>Product Designer</p>
-          <p>User Research, Interaction, Visual design, Prototyping & Testing</p>
+          <h3 className="font-Futura-PT uppercase text-gray-500 mb-5 max-lg:mt-5">Role</h3>
+          <p className="font-Futura-PT text-lg font-semibold">Product Designer</p>
+          <p className="font-Futura-PT">User Research, Interaction, Visual design, Prototyping & Testing</p>
         </div>
       </div>
       </section>
       <section className="mb-10">
       <div className="flex flex-col lg:px-[250px] py-[20px] max-lg:px-[20px] bg-slate-200">
         <div>
-          <h1>Project Goals</h1>
-          <ul>Redesign the website to reflect Le Waf's premium branding and artisanal essence.</ul>
-          <ul>Create a user-friendly and visually appealing ecommerce platform to boost online sales.</ul>
-          <ul>Enhance the overall user experience to align with the brand's reputation for excellence.</ul>
-          <ul>Establish credibility and trust among visitors, emphasizing the brand's recognition on the food network show.</ul>
+          <h1 className="font-Futura-PT-Bold text-3xl mt-10">Project Goals</h1>
+          <li className="font-Futura-PT font-semibold mt-5">Redesign the website to reflect Le Waf's premium branding and artisanal essence.</li>
+          <li className="font-Futura-PT font-semibold  mt-5">Create a user-friendly and visually appealing ecommerce platform to boost online sales.</li>
+          <li className="font-Futura-PT font-semibold  mt-5">Enhance the overall user experience to align with the brand's reputation for excellence.</li>
+          <li className="font-Futura-PT font-semibold  mt-5">Establish credibility and trust among visitors, emphasizing the brand's recognition on the food network show.</li>
         </div>
         <div>
-          <h3>Research and Discovery</h3>
-          <p>I started by conducting a thorough analysis of Le Waf's current website, as well as competitor analysis of other premium artisanal food brands. I also reviewed customer feedback and user behavior data to identify pain points and opportunities for improvement.</p>
-          <h3>Design Approach</h3>
-          <p>Through the process of sketching and exploring page organization, I discovered that the optimal approach was to utilize well-defined modules with abundant white space. Additionally, I identified consistent design patterns tailored for seamless information browsing. My design strategy revolved around elevating the brand's image through elegant visuals, intuitive navigation, and a focus on storytelling. I aimed to create an immersive experience that resonates with the premium nature of Le Waf's artisanal waffles. Two key personas guided my design decisions to ensure the new website would cater to the needs of Le Waf's target audience.</p>
+          <p className="font-Futura-PT mt-5">I started by conducting a thorough analysis of Le Waf's current website, as well as competitor analysis of other premium artisanal food brands. I also reviewed customer feedback and user behavior data to identify pain points and opportunities for improvement.</p>
+          <p className="font-Futura-PT my-5">Through the process of sketching and exploring page organization, I discovered that the optimal approach was to utilize well-defined modules with abundant white space. Additionally, I identified consistent design patterns tailored for seamless information browsing. My design strategy revolved around elevating the brand's image through elegant visuals, intuitive navigation, and a focus on storytelling. I aimed to create an immersive experience that resonates with the premium nature of Le Waf's artisanal waffles. Two key personas guided my design decisions to ensure the new website would cater to the needs of Le Waf's target audience.</p>
         </div>
-        <img src="/images/lewafsketches.jpg" alt="Le Waf low fidelity wireframes"/>
+        <img className="w-9/12 mt-5 mb-10" src="/images/lewafsketches.jpg" alt="Le Waf low fidelity wireframes"/>
       </div>
       </section>
-      <section className="lg:px-[250px] max-lg:px-[20px] mb-10">
-        <h1>Personas</h1>
-        <div className="grid grid-flow-col gap-40 justify-center items-center">
+      <section className="lg:px-[250px] max-lg:px-[20px] py-10 mb-10">
+        <h1 className="font-Futura-PT-Bold text-3xl mb-10">Personas</h1>
+        <div className="lg:grid lg:grid-flow-col lg:gap-40 justify-center items-center mb-10">
         <div>
-          <h3>Persona 1: Gourmet Grace</h3>
-          <h3>Background:</h3>
-          <p>Grace is a 32-year-old professional food enthusiast and culinary connoisseur. She follows popular food shows, reads food blogs, and loves exploring artisanal and gourmet food options. Grace values the quality of ingredients and the craftsmanship that goes into creating unique culinary experiences.</p>
-          <h3>Goals and Behaviors:</h3>
-          <p>Seeks out premium and artisanal food brands.</p>
-          <p>Enjoys experimenting with new flavors and culinary creations.</p>
-          <p>Values brands with a strong story and commitment to quality.</p>
-          <p>Prefers to shop online for convenience but expects a seamless and premium experience.</p>
-          <h3>To address her needs, I focused on:</h3>
-          <p>Creating visually appealing visuals and elegant design elements to resonate with Grace's preference for premium brands.</p>
-          <p>Providing detailed product descriptions, highlighting the artisanal craftsmanship that goes into each waffle.</p>
+          <h3 className="font-Futura-PT uppercase text-gray-500 mb-5">Persona 1: Gourmet Grace</h3>
+          <h3 className="font-Futura-PT uppercase text-gray-500 mb-5">Background:</h3>
+          <p className="font-Futura-PT mb-5">Grace is a 32-year-old professional food enthusiast and culinary connoisseur. She follows popular food shows, reads food blogs, and loves exploring artisanal and gourmet food options. Grace values the quality of ingredients and the craftsmanship that goes into creating unique culinary experiences.</p>
+          <h3 className="font-Futura-PT uppercase text-gray-500 mb-5">Goals and Behaviors:</h3>
+          <li className="font-Futura-PT font-semibold mt-5">Seeks out premium and artisanal food brands.</li>
+          <li className="font-Futura-PT font-semibold mt-5">Enjoys experimenting with new flavors and culinary creations.</li>
+          <li className="font-Futura-PT font-semibold mt-5">Values brands with a strong story and commitment to quality.</li>
+          <li className="font-Futura-PT font-semibold my-5">Prefers to shop online for convenience but expects a seamless and premium experience.</li>
+          <h3 className="font-Futura-PT uppercase text-gray-500 mb-5">To address her needs, I focused on:</h3>
+          <p className="font-Futura-PT mb-5">Creating visually appealing visuals and elegant design elements to resonate with Grace's preference for premium brands.</p>
+          <p className="font-Futura-PT mb-5">Providing detailed product descriptions, highlighting the artisanal craftsmanship that goes into each waffle.</p>
         </div>
         <div>
           <img src="/images/GourmetGraceIllustration.jpg" alt="Persona 1 Illustration" className="shadow-xl"/>
         </div>
       </div>
-      <div className="grid grid-flow-col gap-40 justify-center items-center">
+      <div className="lg:grid lg:grid-flow-col lg:gap-40 justify-center items-center">
         <div>
-          <img src="/images/BusyBenIllustration.jpg" alt="Persona 2 Illustration" className="shadow-xl"/>
+          <h3 className="font-Futura-PT uppercase text-gray-500 mb-5">Persona 2: Busy Ben</h3>
+          <h3 className="font-Futura-PT uppercase text-gray-500 mb-5">Background:</h3>
+          <p className="font-Futura-PT mb-5">Ben is a 28-year-old professional working in a fast-paced corporate environment. He often orders food online due to his hectic schedule and values convenience and efficiency. Ben occasionally treats himself to gourmet and high-quality food items as a way to unwind and enjoy small pleasures.</p>
+          <h3 className="font-Futura-PT uppercase text-gray-500 mb-5">Goals and Behaviors:</h3>
+          <li className="font-Futura-PT font-semibold mt-5">Looks for quick and convenient online shopping experiences.</li>
+          <li className="font-Futura-PT font-semibold mt-5">Prefers brands that offer premium options and elevate everyday indulgences.</li>
+          <li className="font-Futura-PT font-semibold mt-5">Appreciates visually appealing presentations and packaging.</li>
+          <li className="font-Futura-PT font-semibold my-5">Values brands that understand his need for quality despite his busy lifestyle.</li>
+          <h3 className="font-Futura-PT uppercase text-gray-500 mb-5">To address his needs, I focused on:</h3>
+          <p className="font-Futura-PT mb-5">Designing a user-friendly and streamlined checkout process for quick and hassle-free online shopping.</p>
+          <p className="font-Futura-PT mb-5">Displaying clear and appetizing product images for easy decision-making, aligning with Ben's need for efficiency.</p>
         </div>
         <div>
-          <h3>Persona 1: Busy Ben</h3>
-          <h3>Background:</h3>
-          <p>Ben is a 28-year-old professional working in a fast-paced corporate environment. He often orders food online due to his hectic schedule and values convenience and efficiency. Ben occasionally treats himself to gourmet and high-quality food items as a way to unwind and enjoy small pleasures.</p>
-          <h3>Goals and Behaviors:</h3>
-          <p>Looks for quick and convenient online shopping experiences.</p>
-          <p>Prefers brands that offer premium options and elevate everyday indulgences.</p>
-          <p>Appreciates visually appealing presentations and packaging.</p>
-          <p>Values brands that understand his need for quality despite his busy lifestyle.</p>
-          <h3>To address his needs, I focused on:</h3>
-          <p>Designing a user-friendly and streamlined checkout process for quick and hassle-free online shopping.</p>
-          <p>Displaying clear and appetizing product images for easy decision-making, aligning with Ben's need for efficiency.</p>
+          <img src="/images/BusyBenIllustration.jpg" alt="Persona 2 Illustration" className="shadow-2xl"/>
         </div>
       </div>
       </section>
       <section className="mb-10">
       <div className="flex flex-col lg:px-[250px] py-[20px] max-lg:px-[20px] bg-slate-200">
         <div>
-          <h1>Developing the Designs</h1>
-          <p>I created my high fidelity mockups in Figma and then imported them into Invision to allow the engineers to inspect the file and export the HTML and CSS code.
-‍
-          I worked very closely with the Front End team to spec out any missing interactions that were not covered in the high fidelity mockups. I conducted a UX review of each front-end ticket that was implemented to ensure it was aligned with the designs before it went live.</p>
+          <h1 className="font-Futura-PT-Bold text-3xl mt-10">Developing the Designs</h1>
+          <p className="font-Futura-PT mt-5">I created my high fidelity mockups in Figma and then imported them into Invision to allow the engineers to inspect the file and export the HTML and CSS code. I worked very closely with the Front End team to spec out any missing interactions that were not covered in the high fidelity mockups. I conducted a UX review of each front-end ticket that was implemented to ensure it was aligned with the designs before it went live.</p>
 
-          <h2>Design Solutions</h2>
-          <h3>Visual Branding:</h3>
-          <p>We crafted a sophisticated and modern visual identity for Le Waf, incorporating a refined color palette that complements their artisanal branding. The logo was redesigned to evoke a sense of handcrafted care, and using elegant typography.</p>
-          <h3>Homepage:</h3>
-          <p>The homepage features a hero banner showcasing an inviting image of Le Waf's signature waffles, accompanied by a tagline emphasizing their artisanal approach. Below the banner, we added a brief narrative about the brand's journey.</p>
-          <h3>Product Showcase:</h3>
-          <p>The product pages were designed with large, high-quality images of the waffles, capturing the essence of the artisanal process. Each product listing includes a detailed description of the ingredients and the care taken in crafting the waffles, resonating with the brand's dedication to quality.</p>
-          <h3>User-friendly Navigation:</h3>
-          <p>We implemented a clear and intuitive navigation system, ensuring visitors can easily explore different waffle flavors, collections, and offers. A persistent cart icon and streamlined checkout process further enhance the user experience.</p>
+          <h2 className="font-Futura-PT text-xl font-semibold mt-10 mb-5">Design Solutions</h2>
+          <h3 className="font-Futura-PT text-lg">Visual Branding:</h3>
+          <p className="font-Futura-PT my-5">We crafted a sophisticated and modern visual identity for Le Waf, incorporating a refined color palette that complements their artisanal branding. The logo was redesigned to evoke a sense of handcrafted care, and using elegant typography.</p>
+          <h3 className="font-Futura-PT text-lg">Homepage:</h3>
+          <p className="font-Futura-PT my-5">The homepage features a hero banner showcasing an inviting image of Le Waf's signature waffles. Below the banner, we added a brief narrative about the brand's journey.</p>
+          <h3 className="font-Futura-PT text-lg">Product Showcase:</h3>
+          <p className="font-Futura-PT my-5">The product pages were designed with large, high-quality images of the waffles, capturing the essence of the artisanal process. Each product listing includes a detailed description of the ingredients and the care taken in crafting the waffles, resonating with the brand's dedication to quality.</p>
+          <h3 className="font-Futura-PT text-lg">User-friendly Navigation:</h3>
+          <p className="font-Futura-PT my-5">We implemented a clear and intuitive navigation system, ensuring visitors can easily explore different waffle flavors, collections, and offers. A persistent cart icon and streamlined checkout process further enhance the user experience.</p>
         </div>
-        <img/>
-        <img/>
-        <img/>
+        <img src="/images/LWHomepage.png" alt="Le Waf Homepage Design" className="w-7/12 mb-5"/>
+        <img src="/images/LWItemPage.png" alt="Le Waf Item Page Design" className="w-7/12 mb-5"/>
+        <img src="/images/LWCartPage.png" alt="Le Waf Cart Page Design" className="w-7/12 mb-5"/>
+        <img src="/images/LWNewAcc.png" alt="Le Waf New Account Page Design" className="w-7/12 mb-5"/>
+        <img src="/images/LWLogin.png" alt="Le Waf Login Design" className="w-7/12 mb-5"/>
       </div>
       </section>
       <section className="mb-10">
         <div className="flex flex-col lg:px-[250px] max-lg:px-[20px]">
-          <h1>Results and Impact</h1>
-          <p>The redesigned Le Waf website successfully elevated the brand's online presence and showcased their premium waffles' artisanal essence. The user-friendly interface and engaging storytelling increased visitor engagement and time spent on the site. As a result, Le Waf experienced a notable increase in online sales and received positive feedback from customers.</p>
-          <h2>Conlusion:</h2>
-          <p>The Le Waf website redesign was a successful endeavor that aligned the brand's online presence with its premium and artisanal identity. The intuitive user experience and captivating visuals established Le Waf as a top choice for connoisseurs seeking high-quality, handcrafted waffles.</p>
+          <h1 className="font-Futura-PT-Bold text-3xl mt-10">Results and Impact</h1>
+          <p className="font-Futura-PT mt-5">The redesigned Le Waf website successfully elevated the brand's online presence and showcased their premium waffles' artisanal essence. The user-friendly interface and engaging storytelling increased visitor engagement and time spent on the site. As a result, Le Waf experienced a notable increase in online sales and received positive feedback from customers.</p>
+          <p className="font-Futura-PT mt-5">Some key takeaways from this project are:</p>
+          <li className="font-Futura-PT mt-5"><strong>Persona-Centric Design.</strong> Incorporating user personas, such as Gourmet Grace and Busy Ben, was pivotal in crafting a user-centered design that resonated with the target audience. This approach should be a fundamental step in all future projects to ensure alignment with users' needs and preferences.</li>
+          <li className="font-Futura-PT mt-5"><strong>Continuous Feedback and Iteration.</strong> Regularly collecting and analyzing user feedback and behavior data can provide valuable insights for ongoing website improvement. This practice should be integrated into post-launch strategies for all projects.</li>
+          <li className="font-Futura-PT mt-5"><strong>High-Quality Imagery.</strong> Large, high-quality images of products are essential for showcasing their details and quality. This lesson underscores the importance of investing in professional photography and imagery in ecommerce design.</li>
         </div>
       </section>
       </>  
