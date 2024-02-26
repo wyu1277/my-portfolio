a78a196ce1f7ab2996729308b54699a00084dd15
Wilson Yu <wyu1277@gmail.com>
2024-02-26 13:22:21 -0500

    Added css variables to standardize design, updated homepage

--- src/app/page.js
+++ src/app/page.js
@@ -9,26 +9,25 @@ import Link from 'next/link'
 export default function Home() {
   return (
    <>
-   <div className='flex justify-center items-center relative'>
-      <Image priority src={heroPattern} className='lg:h-[700px]'/>
-      <div className='absolute flex flex-col justify-center items-center mt-10'>
-        <h1 className='font-Futura-PT-Bold lg:text-5xl lg:my-2 max-lg:text-2xl'>Hello! I am Wilson,</h1>
-        <h1 className='font-Futura-PT lg:text-5xl font-extrabold text-red-600 lg:my-2 max-lg:text-2xl'>a Product Designer & Developer</h1>
-        <h1 className='font-Futura-PT-Bold lg:text-5xl lg:my-2 max-lg:text-2xl'>based in NYC.</h1>
-        <h2 className='font-Futura-PT-Bold lg:text-xl lg:my-10 max-lg:text-sm max-lg:my-3 text-gray-500'>Welcome to WHYDESIGNS!</h2>
+   <div className='flex justify-center items-center relative bg-red-d-1'>
+      <Image priority src={heroPattern} className='lg:h-[900px]'/>
+      <div className='absolute flex flex-col justify-center items-center p-m'>
+        <h1 className='font-Futura-PT-Bold text-2xl text-white max-md:text-xl max-sm:text-l'>Hello! I am Wilson,</h1>
+        <h1 className='font-Futura-PT text-2xl font-extrabold text-white text-center max-md:text-xl max-sm:text-l'>a Product Designer & Developer</h1>
+        <h1 className='font-Futura-PT-Bold text-2xl text-white max-md:text-xl max-sm:text-l'>based in NYC.</h1>
+        <h2 className='font-Futura-PT-Bold text-xl text-white max-md:text-xl max-sm:text-l'>Welcome to WHYDESIGNS!</h2>
       </div>
    </div>
-   <h2 className='lg:px-[450px] pt-[40px] max-lg:px-[20px] font-Futura-PT text-gray-600'>MY RECENT WORK</h2>
-   <div className='flex flex-col justify-between items-center bg-white lg:px-[450px] max-lg:px-[20px]'>
-    <Link href='/le-waf'>
-    <Image priority src={LWBanner} className='lg:my-5 max-lg:my-2 hover:scale-105 transform transition-transform duration-300 ease-in-out hover:shadow-2xl'/>
-    </Link>
-    <Link href='/dancing-deer'>
-    <Image priority src={DDBanner} className='lg:my-5 max-lg:my-2 hover:scale-105 transform transition-transform duration-300 ease-in-out hover:shadow-2xl'/>
-    </Link>
-    <Link href='/plastec-ventilation'>
-    <Image priority src={PBanner} className=' lg:my-5 max-lg:my-2 hover:scale-105 transform transition-transform duration-300 ease-in-out hover:shadow-2xl'/>
-    </Link>
+    <div className='flex flex-col justify-between items-center py-2xl bg-red-d-1 gap-y-l'>
+      <Link href='/le-waf'>
+        <Image priority src={LWBanner} className='hover:scale-105 transform transition-transform duration-300 ease-in-out hover:shadow-2xl'/>
+      </Link>
+      <Link href='/dancing-deer'>
+        <Image priority src={DDBanner} className='hover:scale-105 transform transition-transform duration-300 ease-in-out hover:shadow-2xl'/>
+      </Link>
+      <Link href='/plastec-ventilation'>
+        <Image priority src={PBanner} className='hover:scale-105 transform transition-transform duration-300 ease-in-out hover:shadow-2xl'/>
+      </Link>
    </div>
    </>
   )
